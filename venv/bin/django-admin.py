23940edc2f63a88a93ca386e512566cd5baf3adb
axmetES <fraktsia@gmail.com>
2020-06-28 13:14:57 +0600

    - deleted extra .

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/blasphemous/Desktop/my_projects/django/django-where-to-go/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
