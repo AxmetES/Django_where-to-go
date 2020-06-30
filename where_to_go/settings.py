1b82ac1cef6dddc0cc79218156520fc925c9e4c6
axmetES <fraktsia@gmail.com>
2020-06-30 14:47:48 +0600

    - Add frontend.

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from os.path import join
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -111,5 +113,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.getenv('STATIC_URL', '/static/')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
+                    os.path.join(BASE_DIR, "static/places"),
+                    os.path.join(BASE_DIR, "static/.gitbook/assets"), ]
+
+STATIC_ROOT = os.getenv("STATIC_ROOT")
