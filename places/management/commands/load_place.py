ed7a2148e2a29a11c4bd3f38aa0a090307e5778d
axmetES <fraktsia@gmail.com>
2020-10-15 18:32:41 +0600

    Code review on load_place modul. - Deleted extra debug print. - Deleted extra argument nergs. - Sorted imports. - Renamed parsed to url_fragment. - Deleted extra recurring block of code.

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,19 +1,16 @@
-import pprint
-from io import BytesIO
 import logging
-
-from django.core.management.base import BaseCommand
 import requests
+from django.core.management.base import BaseCommand
 from urllib.parse import urlparse
-from PIL import Image
 from django.core.files.base import ContentFile
+
 from places.models import Place, Image
 
 
 def get_filename(url):
-    parsed = urlparse(url)
-    file = parsed.path.split('/')[-1]
-    return file
+    url_fragments = urlparse(url)
+    file_name = url_fragments.path.split('/')[-1]
+    return file_name
 
 
 def replace_bd_id(images):
@@ -31,14 +28,12 @@ class Command(BaseCommand):
     help = 'Load json file by the url in database.'
 
     def add_arguments(self, parser):
-        parser.add_argument('url', nargs='?', type=str)
+        parser.add_argument('url', type=str)
 
     def handle(self, *args, **options):
         response = requests.get(options['url'])
         response.raise_for_status()
         response_data = response.json()
-        if 'error' in response_data:
-            raise requests.exceptions.HTTPError(response_data['error'])
         title = response_data['title']
         short_description = response_data['description_short']
         long_description = response_data['description_long']
@@ -48,30 +43,17 @@ class Command(BaseCommand):
         place, is_created = Place.objects.update_or_create(title=title, short_description=short_description, lng=lng,
                                                            lat=lat, defaults={'long_description': long_description})
         images_url = response_data['imgs']
-        if is_created:
-            print(is_created)
-            for num, url in enumerate(images_url):
-                image = Image.objects.create(place=place, position=num)
-                response_img = requests.get(url)
-                response_img.raise_for_status()
-                content = ContentFile(response_img.content)
-                file_name = get_filename(url)
-                image.image.save(file_name, content, True)
-        else:
-            print(is_created)
-            new_images = []
+        if not is_created:
             place_images = place.images.all()
-            images = replace_bd_id(place_images)
-            for url in images_url:
-                file_name = get_filename(url)
-                if file_name not in images:
-                    new_images.append(url)
-            for num, url in enumerate(new_images, len(place_images) + 1):
-                image = Image.objects.create(place=place, position=num)
-                response_img = requests.get(url)
-                content = ContentFile(response_img.content)
-                file_name = get_filename(url)
-                image.image.save(file_name, content, True)
+            place_images.delete()
+
+        for num, url in enumerate(images_url):
+            image = Image.objects.create(place=place, position=num)
+            response_img = requests.get(url)
+            response_img.raise_for_status()
+            content = ContentFile(response_img.content)
+            file_name = get_filename(url)
+            image.image.save(file_name, content, True)
 
 
 if __name__ == '__main__':
